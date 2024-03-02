e9dc05483599bf56ea2f1a1845962d260c971283
thePianoKid <gbraden.dev@gmail.com>
2024-03-01 23:46:23 -0500

    Added actions API endpoint

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# r: the mobile phone user will be assigned to move right
+# l: the mobile phone user will be assigned to move left
+# u: the mobile phone user will be assigned to move up
+# s: the mobile phone user will be assigned to shoot
+actions = ["r", "l", "u", "s"]
+
+@app.get("/actions")
+def read_root():
+    if len(actions) > 0:
+        action = actions.pop()
+    else:
+        action = None
+        
+    return {"action": action}
