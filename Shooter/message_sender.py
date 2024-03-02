4203ac55f6bf8097bb9226c72c72158556f2b182
Wilt Moise <wiltmoise@gmail.com>
2024-03-02 16:32:35 -0500

    Debugging issues and testing with randomly generated values

--- Shooter/message_sender.py
+++ Shooter/message_sender.py
@@ -1,7 +1,7 @@
 import threading
 import time
 import queue
-import random
+from random import random
 
 
 def message_sender(outgoing_queue, incoming_queue):
@@ -10,27 +10,34 @@ def message_sender(outgoing_queue, incoming_queue):
     counter = 0
     initial_num = 0
     while True:
-       
-        time.sleep(0.01)  # Simulate delay between sending messages
-        
-        message = letters_l[4]
+        rand = int(random() * (10000 - 0) + 0);
+        if (rand in range(0, 100)):
+            if rand in range(0,10):
+                message = 'r' 
+            elif rand in range(20, 30):
+                message = 'j'
+            elif rand in range(30, 40):
+                message = 'l'
+            else:
+                message = "g"
+            outgoing_queue.put_nowait(message)
 
-        outgoing_queue.put(message)
-        print(f"Sent: {message}")
-        print( "THIS IS THE CURRENT COMMAND: " + letters_l[initial_num])
-        counter += 1
-        initial_num += 1
-        if initial_num == 4:
-            initial_num = 0
+            #print(f"Sent: {message}")
+            #print( "THIS IS THE CURRENT COMMAND: " + letters_l[initial_num])
+            counter += 1
+            initial_num += 1
+            if initial_num == 4:
+                initial_num = 0
 
-        try:
-            # Attempt to receive an "action processed" notification from the game
-            incoming_queue.get_nowait()  # Use non-blocking get
-            counter -= 1  # Decrement the counter when an action is processed
-            print(f"Action processed, counter decremented. New counter: {counter}")
-        except queue.Empty:
-            # No action received, continue
-            pass
+            try:
+                # Attempt to receive an "action processed" notification from the game
+                incoming_queue.get_nowait()  # Use non-blocking get
+                counter -= 1  # Decrement the counter when an action is processed
+                print(f"Action processed, counter decremented. New counter: {counter}")
+            except queue.Empty:
+                # No action received, continue
+                pass
+
     
     
 
--- Shooter/shooter_tut13.py
+++ Shooter/shooter_tut13.py
@@ -6,6 +6,7 @@ from message_sender import start_message_thread
 import csv
 import button
 import queue
+import threading
 
 from pythread import AsyncThreadManager
 
@@ -19,7 +20,7 @@ global input_from_q
 
 # Start the background task
 
-start_message_thread(incoming_queue,outgoing_queue)
+start_message_thread(outgoing_queue,incoming_queue) # This right here... is my 13th reason.
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
@@ -684,212 +685,161 @@ world = World()
 player, health_bar = world.process_data(world_data)
 
 
-
 run = True
 while run:
-	
+  clock.tick(60)
 
-	clock.tick(FPS)
-
-	if start_game == False:
-		#draw menu
-		screen.fill(BG)
-		#add buttons
-		if start_button.draw(screen):
-			start_game = True
-			start_intro = True
-		if exit_button.draw(screen):
-			run = False
-	else:
-		#update background
-		draw_bg()
-		#draw world map
-		world.draw()
-		#show player health
-		health_bar.draw(player.health)
-		#show ammo
-		draw_text('AMMO: ', font, WHITE, 10, 35)
-		for x in range(player.ammo):
-			screen.blit(bullet_img, (90 + (x * 10), 40))
-		#show grenades
-		draw_text('GRENADES: ', font, WHITE, 10, 60)
-		for x in range(player.grenades):
-			screen.blit(grenade_img, (135 + (x * 15), 60))
-
-
-		player.update()
-		player.draw()
-
-		for enemy in enemy_group:
-			enemy.ai()
-			enemy.update()
-			enemy.draw()
-
-		#update and draw groups
-		bullet_group.update()
-		grenade_group.update()
-		explosion_group.update()
-		item_box_group.update()
-		decoration_group.update()
-		water_group.update()
-		exit_group.update()
-		bullet_group.draw(screen)
-		grenade_group.draw(screen)
-		explosion_group.draw(screen)
-		item_box_group.draw(screen)
-		decoration_group.draw(screen)
-		water_group.draw(screen)
-		exit_group.draw(screen)
-
-		#show intro
-		if start_intro == True:
-			if intro_fade.fade():
-				start_intro = False
-				intro_fade.fade_counter = 0
-		
+  if start_game == False:
+    #draw menu
+    screen.fill(BG)
+    #add buttons
+    if start_button.draw(screen):
+      start_game = True
+      start_intro = True
+    if exit_button.draw(screen):
+      run = False
+  else:
+    #print("here2")
+    #update background
+    draw_bg()
+    #draw world map
+    world.draw()
+    #show player health
+    health_bar.draw(player.health)
+    #show ammo
+    draw_text('AMMO: ', font, WHITE, 10, 35)
+    for x in range(player.ammo):
+      screen.blit(bullet_img, (90 + (x * 10), 40))
+    #show grenades
+    draw_text('GRENADES: ', font, WHITE, 10, 60)
+    for x in range(player.grenades):
+      screen.blit(grenade_img, (135 + (x * 15), 60))
 
 
-		#update player actions
-		if player.alive:
-			input_from_q = incoming_queue.get()
-	
-			
+    player.update()
+    player.draw()
 
+    for enemy in enemy_group:
+      enemy.ai()
+      enemy.update()
+      enemy.draw()
 
+    #update and draw groups
+    bullet_group.update()
+    grenade_group.update()
+    explosion_group.update()
+    item_box_group.update()
+    decoration_group.update()
+    water_group.update()
+    exit_group.update()
+    bullet_group.draw(screen)
+    grenade_group.draw(screen)
+    explosion_group.draw(screen)
+    item_box_group.draw(screen)
+    decoration_group.draw(screen)
+    water_group.draw(screen)
+    exit_group.draw(screen)
 
-				
-			#shoot bullets
-			if shoot:
+    #show intro
+    if start_intro == True:
+      if intro_fade.fade():
+        start_intro = False
+        intro_fade.fade_counter = 0
 
+    #update player actions
+    if player.alive:
+      #shoot bullets
+      if shoot:
+        #outgoing_queue.put("shoot")
+        #print("Shoot initiated")
+        try:	
+          message = outgoing_queue.get_nowait()
+          #print(f"Space UAU Button was Pressed: {message}")
+        
+        except queue.Empty:
+          c = 4
+          #print("No message received because the queue is empty")
+        #print(message)
+      #throw grenades
+      elif grenade and grenade_thrown == False and player.grenades > 0:
+        grenade = Grenade(player.rect.centerx + (0.5 * player.rect.size[0] * player.direction),\
+              player.rect.top, player.direction)
+        grenade_group.add(grenade)
+        #reduce grenades
+        player.grenades -= 1
+        grenade_thrown = True
+      if player.in_air:
+        player.update_action(2)#2: jump
+      elif moving_left or moving_right:
+        player.update_action(1)#1: run
+        outgoing_queue.put("run")
+        #print("run initiated")
+        try:	
+          message = outgoing_queue.get_nowait()
+          #print(f"W Button was Pressed: {message}")
+        
+        except queue.Empty:
+          b = 2
+          #print("No message received because the queue is empty")
+      else:
+        player.update_action(0)#0: idle
+      screen_scroll, level_complete = player.move(moving_left, moving_right)
+      bg_scroll -= screen_scroll
+      #check if player has completed the level
+      if level_complete:
+        start_intro = True
+        level += 1
+        bg_scroll = 0
+        world_data = reset_level()
+        if level <= MAX_LEVELS:
+          #load in level data and create world
+          with open(f'level{level}_data.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for x, row in enumerate(reader):
+              for y, tile in enumerate(row):
+                world_data[x][y] = int(tile)
+          world = World()
+          player, health_bar = world.process_data(world_data)	
+    else:
+      screen_scroll = 0
+      if death_fade.fade():
+        if restart_button.draw(screen):
+          death_fade.fade_counter = 0
+          start_intro = True
+          bg_scroll = 0
+          world_data = reset_level()
+          #load in level data and create world
+          with open(f'level{level}_data.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for x, row in enumerate(reader):
+              for y, tile in enumerate(row):
+                world_data[x][y] = int(tile)
+          world = World()
+          player, health_bar = world.process_data(world_data)
 
-				
-				#outgoing_queue.put("shoot")
-				#print("Shoot initiated")
-				try:	
-					message = outgoing_queue.get_nowait()
-					#print(f"Space UAU Button was Pressed: {message}")
-				
-				except queue.Empty:
-					c = 4
-					#print("No message received because the queue is empty")
-				#print(message)
-			#throw grenades
-			elif grenade and grenade_thrown == False and player.grenades > 0:
-				grenade = Grenade(player.rect.centerx + (0.5 * player.rect.size[0] * player.direction),\
-				 			player.rect.top, player.direction)
-				grenade_group.add(grenade)
-				#reduce grenades
-				player.grenades -= 1
-				grenade_thrown = True
-			if player.in_air:
-				player.update_action(2)#2: jump
-			elif moving_left or moving_right:
-				player.update_action(1)#1: run
-				outgoing_queue.put("run")
-				#print("run initiated")
-				try:	
-					message = outgoing_queue.get_nowait()
-					#print(f"W Button was Pressed: {message}")
-				
-				except queue.Empty:
-					b = 2
-					#print("No message received because the queue is empty")
-			else:
-				player.update_action(0)#0: idle
-			screen_scroll, level_complete = player.move(moving_left, moving_right)
-			bg_scroll -= screen_scroll
-			#check if player has completed the level
-			if level_complete:
-				start_intro = True
-				level += 1
-				bg_scroll = 0
-				world_data = reset_level()
-				if level <= MAX_LEVELS:
-					#load in level data and create world
-					with open(f'level{level}_data.csv', newline='') as csvfile:
-						reader = csv.reader(csvfile, delimiter=',')
-						for x, row in enumerate(reader):
-							for y, tile in enumerate(row):
-								world_data[x][y] = int(tile)
-					world = World()
-					player, health_bar = world.process_data(world_data)	
-		else:
-			screen_scroll = 0
-			if death_fade.fade():
-				if restart_button.draw(screen):
-					death_fade.fade_counter = 0
-					start_intro = True
-					bg_scroll = 0
-					world_data = reset_level()
-					#load in level data and create world
-					with open(f'level{level}_data.csv', newline='') as csvfile:
-						reader = csv.reader(csvfile, delimiter=',')
-						for x, row in enumerate(reader):
-							for y, tile in enumerate(row):
-								world_data[x][y] = int(tile)
-					world = World()
-					player, health_bar = world.process_data(world_data)
-	
-	
-	
-	
+    input_from_q = outgoing_queue.get_nowait()
+    if(input_from_q != queue.Empty):
+      if input_from_q != None:
+        if input_from_q == 'l':
+          moving_left = True
+          moving_right = False
+        elif input_from_q == 'r':
+          moving_right = True # My 14th reason why
+          moving_left = False;
+        elif input_from_q == 's':
+          shoot = True
+        elif input_from_q == 'g':
+          player.jump = True
+        elif input_from_q == 'j':
+          player.jump = True
 
-
-	if input_from_q != None:
-		if input_from_q == 'l':
-			moving_left = True
-			print(" I RECEIVED LEFT")
-		elif input_from_q == 'r':
-			moving_right == True
-			print(" I RECEIVED RIGHT")
-		
-		elif input_from_q == 's':
-			shoot = True
-			print(" I RECEIVED SHOOT")
-		
-		
-		elif input_from_q == 'g':
-			player.jump = True
-			print("I RECEIVED SHOOT")
-		
-		elif input_from_q == 'j':
-			player.jump = True
-			print(" I RECEIVED JUMP")
-
-	for event in pygame.event.get():
-		#quit game
-		if event.type == pygame.QUIT:
-			run = False
-		#keyboard presses
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_a:
-				moving_left = True
-			if event.key == pygame.K_d:
-				moving_right = True
-			if event.key == pygame.K_SPACE:
-				shoot = True
-			if event.key == pygame.K_q:
-				grenade = True
-			if event.key == pygame.K_w:
-				player.jump = True
-				jump_fx.play()
-			if event.key == pygame.K_ESCAPE:
-				run = False
-
-
-		#keyboard button released
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_a:
-				moving_left = False
-			if event.key == pygame.K_d:
-				moving_right = False
-			if event.key == pygame.K_SPACE:
-				shoot = False
-			if event.key == pygame.K_q:
-				grenade = False
-				grenade_thrown = False
-
-
-	pygame.display.update()
-
+  for event in pygame.event.get():
+      #quit game
+      if event.type == pygame.QUIT:
+        run = False
+      if event.type == pygame.KEYDOWN:
+        pass
+      if event.type == pygame.KEYUP:
+        pass
+  pygame.display.update()
 pygame.quit()
